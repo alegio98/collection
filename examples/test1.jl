5c6bbd83b235d2cb851384bbe25eee85c07dbf6c
alegio98 <alessandrogiovannini7@yahoo.it>
2021-01-24 16:52:24 +0100

    test big data es

---
+++ examples/test1.jl
@@ -0,0 +1,57 @@
+using Test
+using LinearAlgebraicRepresentation
+Lar = LinearAlgebraicRepresentation
+
+#aggiunta da me
+
+@testset "cuboid Tests" begin
+	square=Lar.cuboid([1,1])
+	cube=Lar.cuboid([1,1,1])
+	@testset "cuboid Tests 2D" begin
+		@test typeof(square)==Tuple{Array{Float64,2},Array{Array{Int64,1},1}}
+		@test length(square)==2
+		@test size(square[1])==(2, 4)
+	end
+	@testset "cuboid Tests 3D" begin
+		@test typeof(cube)==Tuple{Array{Float64,2},Array{Array{Int64,1},1}}
+		@test length(cube)==2
+		@test size(cube[1])==(3, 8)
+	end
+end
+
+@testset "2D" begin
+	square = ([[0.; 0] [0; 1] [1; 0] [1; 1]], [[1, 2, 3,
+			4]], [[1,2], [1,3], [2,4], [3,4]])
+	@testset "apply Translation 2D" begin
+		@test typeof(Lar.apply(Lar.t(-0.5,-0.5),square))==Tuple{Array{Float64,2},Array{Array{Int64,1},1},Array{Array{Int64,1},1}}
+		@test Lar.apply(Lar.t(-0.5,-0.5),square)==([-0.5 -0.5 0.5 0.5; -0.5 0.5 -0.5 0.5], Array{Int64,1}[[1, 2, 3, 4]], Array{Int64,1}[[1, 2], [1, 3], [2, 4], [3, 4]])
+	end
+	@testset "apply Scaling 2D" begin
+		@test typeof(Lar.apply(Lar.s(-0.5,-0.5),square))==Tuple{Array{Float64,2},Array{Array{Int64,1},1},Array{Array{Int64,1},1}}
+		@test Lar.apply(Lar.s(-0.5,-0.5),square)==([0.0 0.0 -0.5 -0.5; 0.0 -0.5 0.0 -0.5], Array{Int64,1}[[1, 2, 3, 4]], Array{Int64,1}[[1, 2], [1, 3], [2, 4], [3, 4]])
+	end
+	@testset "apply Rotation 2D" begin
+		@test typeof(Lar.apply(Lar.r(0),square))==Tuple{Array{Float64,2},Array{Array{Int64,1},1},Array{Array{Int64,1},1}}
+		@test Lar.apply(Lar.r(0),square)==([0.0 0.0 1.0 1.0; 0.0 1.0 0.0 1.0], Array{Int64,1}[[1, 2, 3, 4]], Array{Int64,1}[[1, 2], [1, 3], [2, 4], [3, 4]])
+	end
+end
+
+
+@testset "3D" begin
+	cube=Lar.cuboid([1,1,1])
+	@testset "apply Translation 3D" begin
+		@test typeof(Lar.apply(Lar.t(-0.5,-0.5,-0.5),cube))==Tuple{Array{Float64,2},Array{Array{Int64,1},1}}
+		@test Lar.apply(Lar.t(-0.5, -0.5, -0.5),cube) ==
+		([-0.5 -0.5 -0.5 -0.5 0.5 0.5 0.5 0.5;
+		-0.5 -0.5 0.5 0.5 -0.5 -0.5 0.5 0.5; -0.5 0.5 -0.5 0.5 -0.5 0.5 -0.5 0.5],
+		Array{Int64,1}[[1, 2, 3, 4, 5, 6, 7, 8]])
+	end
+	@testset "apply Scaling 3D" begin
+		@test typeof(Lar.apply(Lar.s(-0.5,-0.5,-0.5),cube))==
+		Tuple{Array{Float64,2},Array{Array{Int64,1},1}}
+		@test Lar.apply(Lar.s(-0.5, -0.5, -0.5),cube) ==
+		([0.0 0.0 0.0 0.0 -0.5 -0.5 -0.5 -0.5;
+		0.0 0.0 -0.5 -0.5 0.0 0.0 -0.5 -0.5; 0.0 -0.5 0.0 -0.5 0.0 -0.5 0.0 -0.5],
+		Array{Int64,1}[[1, 2, 3, 4, 5, 6, 7, 8]])
+	end
+end
